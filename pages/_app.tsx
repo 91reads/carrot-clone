8601474974c14a4934b04a151d236c60563101c1
priscolatrans <priscolatrans@gmail.com>
2022-04-26 00:51:40 +0900

    Feat: SWRConfig 설정. 공용 fetcher

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SWRConfig } from 'swr';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SWRConfig
+      value={{
+        fetcher: (url: string) => fetch(url).then(res => res.json())
+      }}>
+      <Component {...pageProps} />
+    </SWRConfig>
+  )
 }
 
 export default MyApp
