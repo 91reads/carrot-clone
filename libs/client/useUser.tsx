fa11e215e72bf2b80b314a5098797643335dd87e
priscolatrans <priscolatrans@gmail.com>
2022-04-26 00:51:57 +0900

    Feat: useUser hook 에 swr 적용

---
+++ libs/client/useUser.tsx
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import useSWR from 'swr';
+
+export default function useUser() {
+  const { data, error } = useSWR('/api/users/me');
+  const router = useRouter();
+
+  useEffect(() => {
+    if(data && !data.ok) {
+      router.replace('/enter');
+    }
+  }, [data, router]);
+
+  return {user: data?.profile, isLoading: !data && !error};
+}
