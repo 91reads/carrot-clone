d16c95f77d669181d68fb1b62b7011e24f760725
priscolatrans <priscolatrans@gmail.com>
2022-04-30 21:44:28 +0900

    ✨ Feat: Products 페이지에 실 데이터 업로드, 불러오기 적용

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,8 +1,41 @@
 import type { NextPage } from "next";
 import Button from "@components/Button";
 import Layout from "@components/Layout";
+import { useRouter } from "next/router";
+import useSWR, { useSWRConfig } from "swr";
+import Link from "next/link";
+import { Product, User } from "@prisma/client";
+import useMutation from "@libs/client/useMutation";
+import { cls } from "@libs/server/utils";
+import useUser from "@libs/client/useUser";
+
+interface ProductWithuser extends Product {
+  user: User;
+}
+interface ItemDetailResponse {
+  ok: boolean;
+  product: ProductWithuser;
+  relatedProducts: Product[];
+  isLiked: boolean;
+}
 
 const ItemDetail: NextPage = () => {
+  const router = useRouter();
+  const { user, isLoading } = useUser();
+  // useSWR Optional Query
+  const { mutate } = useSWRConfig();
+  const { data, mutate: boundMutate } = useSWR<ItemDetailResponse>(router.query.id ? `/api/products/${router.query.id}` : null);
+  const [toggleFav] = useMutation(`/api/products/${router.query.id}/fav`);
+  const onFavClick = () => {
+    toggleFav({});
+    if(!data) return;
+    boundMutate((prev) => prev && { ...data, isLiked: !data?.isLiked }, false);
+    // mutate({ ...data, isLiked: !data?.isLiked }, false);
+    // mutate('/api/users/me', (prev:any) => ({ ok: !prev.ok }), false)
+  }
+
+  if (!data) return <div>...loading</div>;
+
   return (
     <Layout canGoBack>
       <div className="px-4  py-4">
@@ -11,43 +44,50 @@ const ItemDetail: NextPage = () => {
           <div className="flex cursor-pointer py-3 border-t border-b items-center space-x-3">
             <div className="w-12 h-12 rounded-full bg-slate-300" />
             <div>
-              <p className="text-sm font-medium text-gray-700">Steve Jebs</p>
-              <p className="text-xs font-medium text-gray-500">
-                View profile &rarr;
-              </p>
+              <p className="text-sm font-medium text-gray-700">{data.product.user.name}</p>
+              <Link passHref href={`/users/profiles/${data.product.user.id}`}>
+                <p className="text-xs font-medium text-gray-500">
+                  View profile &rarr;
+                </p>
+              </Link>
             </div>
           </div>
           <div className="mt-5">
-            <h1 className="text-3xl font-bold text-gray-900">Galaxy S50</h1>
-            <span className="text-2xl block mt-3 text-gray-900">$140</span>
+            <h1 className="text-3xl font-bold text-gray-900">{data.product.name}</h1>
+            <span className="text-2xl block mt-3 text-gray-900">${data.product.price}</span>
             <p className=" my-6 text-gray-700">
-              My money&apos;s in that office, right? If she start giving me some
-              bullshit about it ain&apos;t there, and we got to go someplace
-              else and get it, I&apos;m gonna shoot you in the head then and
-              there. Then I&apos;m gonna shoot that bitch in the kneecaps, find
-              out where my goddamn money is. She gonna tell me too. Hey, look at
-              me when I&apos;m talking to you, motherfucker. You listen: we go
-              in there, and that ni**a Winston or anybody else is in there, you
-              the first motherfucker to get shot. You understand?
+              {data.product.description}
             </p>
             <div className="flex items-center justify-between space-x-2">
               <Button large text="Talk to seller" />
-              <button className="p-3 rounded-md flex items-center justify-center text-gray-400 hover:bg-gray-100 hover:text-gray-500">
-                <svg
-                  className="h-6 w-6 "
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                  />
-                </svg>
+              <button
+                onClick={onFavClick}
+                className={cls(
+                  "p-3 rounded-md flex items-center justify-center",
+                  data.isLiked ? "text-red-500 hover:text-red-600" : "text-gray-400 hover:text-gray-500"
+                )}>
+                {data.isLiked
+                  ? (
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
+                  </svg>) 
+                  : (
+                    <svg
+                      className="h-6 w-6 "
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 28 28"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                      />
+                    </svg>
+                  )}
               </button>
             </div>
           </div>
@@ -55,17 +95,19 @@ const ItemDetail: NextPage = () => {
         <div>
           <h2 className="text-2xl font-bold text-gray-900">Similar items</h2>
           <div className=" mt-6 grid grid-cols-2 gap-4">
-            {[1, 2, 3, 4, 5, 6].map((_, i) => (
-              <div key={i}>
-                <div className="h-56 w-full mb-4 bg-slate-300" />
-                <h3 className="text-gray-700 -mb-1">Galaxy S60</h3>
-                <span className="text-sm font-medium text-gray-900">$6</span>
-              </div>
+            {data.relatedProducts.map((product) => (
+              <Link key={product.id} passHref href={`/products/${product.id}`}>
+                <div>
+                  <div className="h-56 w-full mb-4 bg-slate-300" />
+                  <h3 className="text-gray-700 -mb-1">{product.name}</h3>
+                  <span className="text-sm font-medium text-gray-900">${product.price}</span>
+                </div>
+              </Link>
             ))}
           </div>
         </div>
       </div>
-    </Layout>
+    </Layout >
   );
 };
 
