59b8834923afea28d798f237810b77c079f19635
priscolatrans <priscolatrans@gmail.com>
2022-05-04 09:29:48 +0900

    ✨ Feat: 동네 생활 포스트 API 추가 좋아요, 궁금해요 포함

---
+++ pages/api/posts/index.ts
@@ -0,0 +1,79 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/client/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  if (req.method === "POST") {
+    const {
+      body: { question, latitude, longitude },
+      session: { user },
+    } = req;
+    const post = await client.post.create({
+      data: {
+        question,
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+      },
+    });
+    res.json({
+      ok: true,
+      post,
+    });
+  }
+
+  if (req.method === "GET") {
+    const {
+      query: { latitude, longitude },
+    } = req;
+
+    const parsedLatitude = parseFloat(latitude.toString());
+    const parsedLongitude = parseFloat(longitude.toString());
+    
+    const posts = await client.post.findMany({
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+            avatar: true,
+          },
+        },
+        _count: {
+          select: {
+            wondering: true,
+            answers: true,
+          },
+        },
+      },
+      where: {
+        latitude: {
+          gte: parsedLatitude - 0.01,
+          lte: parsedLatitude + 0.01
+        },
+        longitude: {
+          gte: parsedLongitude - 0.01,
+          lte: parsedLongitude + 0.01,
+        }
+      }
+    });
+    res.json({
+      ok: true,
+      posts,
+    });
+  }
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["POST", "GET"],
+    handler,
+    isPrivate: true,
+  })
+);
