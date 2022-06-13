2cf2356899c27c0f8ac581684a61f68ff8204a46
priscolatrans <priscolatrans@gmail.com>
2022-06-13 20:55:34 +0900

    ✨ Feat: 회원변경 백앤드 로직 추가

--- pages/api/users/me/index.tsx
+++ pages/api/users/me/index.tsx
@@ -5,20 +5,104 @@ import { withApiSession } from '@libs/server/withSession';
 
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
 
-  console.log(req.session.user);
+  if (req.method === 'GET') {
+    const profile = await client.user.findUnique({
+      where: {
+        id: req.session.user?.id,
+      }
+    })
 
-  const profile = await client.user.findUnique({
-    where: {
-      id: req.session.user?.id,
+    res.json({ ok: true, profile })
+  }
+
+  if (req.method === "POST") {
+    const {
+      session: { user },
+      body: { email, phone, name },
+    } = req;
+
+    const currentUser = await client.user.findUnique({
+      where: {
+        id: user?.id,
+      },
+    });
+
+    if (email && email !== currentUser?.email) {
+      const alreadyExists = Boolean(
+        await client.user.findUnique({
+          where: {
+            email,
+          },
+          select: {
+            id: true
+          }
+        })
+      );
+
+      if (alreadyExists) {
+        return res.json({
+          ok: false,
+          error: "Email already taken."
+        })
+      }
+      await client.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          email,
+        }
+      })
+      res.json({ ok: true })
     }
-  })
 
-  res.json({ ok: true, profile })
+    if(phone && phone !== currentUser?.phone) {
+      const alreadyExists = Boolean(
+        await client.user.findUnique({
+          where: {
+            phone,
+          },
+          select: {
+            id: true
+          }
+        })
+      );
+
+      if (alreadyExists) {
+        return res.json({
+          ok: false,
+          error: "Phone already taken."
+        })
+      }
+      await client.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          phone,
+        }
+      })
+      res.json({ ok: true })
+    }
+
+    if(name) {
+      await client.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          name,
+        }
+      })
+    }
+    
+    res.json({ ok: true })
+  }
 }
 
 
 export default withApiSession(withHandler({
-  methods: ['GET'],
+  methods: ['GET', 'POST'],
   handler,
   isPrivate: true,
 }));
