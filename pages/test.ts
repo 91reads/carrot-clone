e42e4e8afcffeab03876fbd25bcfe9d658ebf553
priscolatrans <priscolatrans@gmail.com>
2022-06-15 17:39:17 +0900

    ✨ Feat: API middleware 주석처리

---
+++ pages/test.ts
@@ -0,0 +1,13 @@
+// import type { NextRequest, NextFetchEvent } from "next/server";
+// import { NextResponse } from "next/server";
+// export function middleware(req: NextRequest, ev: NextFetchEvent) {
+//   if (req.ua?.isBot) {
+//     return new Response("Plz don't be a bot. Be human.", { status: 403 });
+//   }
+//   if (!req.url.includes("/api")) {
+//     if (!req.url.includes("/enter") && !req.cookies.carrotsession) {
+//       return NextResponse.redirect(new URL("/enter", req.url));
+//     }
+//   }
+//   //  return NextResponse.json({ ok: true });
+// }
