ac8cdd6a30680ad4b54595268f281bd595098a28
priscolatrans <priscolatrans@gmail.com>
2022-06-22 02:25:06 +0900

    ✨ Feat: 외부 callAPI 추가

--- libs/front-api/api.ts
+++ libs/front-api/api.ts
@@ -1,24 +1,43 @@
-
+interface HeaderParams {
+  "Content-Type": string;
+  Authorization: string;
+}
 interface APIParams {
   uri: string;
-  method: 'POST' | 'GET',
+  headers?: HeaderParams;
+  method: "POST" | "GET";
   body: any;
 }
 
-export const callAPI = async ({ uri, method, body }: APIParams) => {
-
-  console.log('callAPI:',uri, method, body);
-
-  const fetch_result = await fetch(uri ,{
-    headers: {
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
+export const callAPI = async ({ uri, headers, method, body }: APIParams) => {
+  console.log("callAPI:", uri, method, body);
+  
+  const fetch_result = await fetch(uri, {
+    headers: { "Content-Type": "application/json; charset=UTF-8" },
     body: JSON.stringify(body),
     method,
-  })
+  });
+  
+  const fetch_json = await fetch_result.json();
+  
+  console.log(fetch_json);
+  
+  
+  if (fetch_json.ok === false) throw new Error("login error");
+  return fetch_json.data;
+};
+
+export const callCloudFlareAPI = async ({ uri, headers, method, body }: APIParams) => {
+  console.log("callCloudFlareAPI:", uri, method, body);
+
+  const fetch_result = await fetch(uri, {
+    headers: { "Content-Type": "application/json; charset=UTF-8" },
+    body: JSON.stringify(body),
+    method,
+  });
 
   const fetch_json = await fetch_result.json();
 
-  if(fetch_json.ok === false) throw new Error('login error');
-  return fetch_json.data;
-}
+  if (fetch_json.ok === false) throw new Error("login error");
+  return fetch_json;
+};
