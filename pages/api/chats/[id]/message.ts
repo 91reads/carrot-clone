f730a2710319d5b889ebf53dfda4d919c1394f21
priscolatrans <priscolatrans@gmail.com>
2022-06-26 17:51:14 +0900

    ✨ Feat: 백앤드 - 메시지 관련 API 추가. TODO: 판매자와 문의자 외 접근 제외

---
+++ pages/api/chats/[id]/message.ts
@@ -0,0 +1,86 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/client/client";
+import { withApiSession } from '@libs/server/withSession';
+
+async function handler (req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+  
+  
+  if (req.method === 'GET') {
+    const { 
+      query: { id }, 
+    } = req;
+    const room_message = await client.chat.findUnique({
+      where: {
+        id: Number(id),
+      },
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+            avatar: true,
+          }
+        },
+        messages: {
+          select: {
+            message: true,
+            user: {
+              select: {
+                id: true,
+                name: true,
+                avatar: true,
+              }
+            }
+          }
+        }
+      }
+    })
+    
+    res.json({
+      ok: true,
+      data: room_message,
+    })
+  }
+  
+  if (req.method === 'POST') {
+    const { 
+      session: { user },
+      body: { chat_id, message, product_id }
+    } = req;
+
+    const newMessage = await client.message.create({
+      data: {
+        message: message,
+        chat: {
+          connect: {
+            id: Number(chat_id),
+          },
+        },
+        product: {
+          connect: {
+            id: Number(product_id),
+          }
+        },
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+      }
+    });
+
+    res.json({
+      ok: true,
+      data: newMessage,
+    })
+  }
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["GET", "POST"],
+    handler,
+    isPrivate: true,
+  })
+)
