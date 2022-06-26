ff34e6462f339ce165bc319c48ca3a7af93c7e99
priscolatrans <priscolatrans@gmail.com>
2022-06-26 17:42:15 +0900

    🔥 Remove: 안쓰는 콘솔 로그 삭제.

--- libs/front-api/api.ts
+++ libs/front-api/api.ts
@@ -12,29 +12,30 @@ interface APIParams {
 }
 
 export const callAPI = async ({ uri, headers, method, body }: APIParams) => {
-  console.log("callAPI:", uri, method, body);
 
-  const login_check = sessionStorage.getItem('userId')
-  if(!login_check) {
-    Router.push('/enter')
+  const login_check = sessionStorage.getItem("userId");
+  if (!login_check) {
+    Router.push("/enter");
   }
-  
+
   const fetch_result = await fetch(uri, {
     headers: { "Content-Type": "application/json; charset=UTF-8" },
     body: JSON.stringify(body),
     method,
   });
-  
+
   const fetch_json = await fetch_result.json();
-  // console.log('페치한 데이터를 정상적으로 받음:', fetch_json)
-  
+
   if (fetch_json.ok === false) throw new Error("login error");
-  // console.log('오류없이 최종적으로 데이터를 보냄:', fetch_json.data)
   return fetch_json.data;
 };
 
-export const callCloudFlareAPI = async ({ uri, headers, method, body }: APIParams) => {
-  console.log("callCloudFlareAPI:", uri, method, body);
+export const callCloudFlareAPI = async ({
+  uri,
+  headers,
+  method,
+  body,
+}: APIParams) => {
 
   const fetch_result = await fetch(uri, {
     headers: { "Content-Type": "application/json; charset=UTF-8" },
