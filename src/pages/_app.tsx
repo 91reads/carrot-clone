a6910211814d7b65ec49a04f0298ecf40b41e9f3
priscolatrans <priscolatrans@gmail.com>
2022-06-27 01:25:32 +0900

    ✨ Feat: 글로벌 스타일 추가 + styled-component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,57 @@
 import type { AppProps } from 'next/app'
 import { SWRConfig } from 'swr';
+import styled, { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+import Tabbar from 'src/components/Layout/Tabbar';
+import Appbar from 'src/components/Layout/Appbar';
+
+const GlobalStyles = createGlobalStyle`
+  :root {
+    --br-6: 0.6rem;
+
+    --primary: #ff7e35;
+
+    --gray-1: #e7e7e7;
+    --gray-2: #ccc;
+    --gray-3: #706d6c;
+    --gray-4: #33190a;
+
+    --weight-500: 500;
+    --weight-600: 600;
+    --weight-700: 700;
+  }
+  ${reset}
+  * {
+    box-sizing: border-box;
+    font-family: Pretendard !important;
+  }
+  html {
+    font-size: 10px;
+  }
+`;
+
+const AppLayout = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const AppInnerLayout = styled.div`
+  position: relative;
+  max-height: 91.5rem;
+  max-width: 41.2rem;
+  /* padding: 2rem; */
+  overflow: scroll;
+
+  padding-bottom: 6rem;
+
+  width: 100%;
+  height: 100%;
+
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -7,7 +59,14 @@ function MyApp({ Component, pageProps }: AppProps) {
       value={{
         fetcher: (url: string) => fetch(url).then(res => res.json())
       }}>
-      <Component {...pageProps} />
+      <GlobalStyles />
+      <AppLayout>
+        <AppInnerLayout>
+          {/* <Appbar /> */}
+          <Component {...pageProps} />
+          <Tabbar />
+        </AppInnerLayout>
+      </AppLayout>
     </SWRConfig>
   )
 }
