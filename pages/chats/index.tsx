04fce080c2d66a66bbd080c994396ba6988d9346
priscolatrans <priscolatrans@gmail.com>
2022-06-26 17:49:40 +0900

    ✨ Feat: 채팅방 목록 추가.

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,23 +1,64 @@
-import type { NextPage } from "next";
 import Link from "next/link";
 import Layout from "@components/Layout";
+import useSWR from "swr";
+import { getChatList } from "@libs/front-api/chat";
+import { useRouter } from "next/router";
 
-const Chats: NextPage = () => {
+interface MessageStructureType {
+  chatId: number;
+  createdAt: string;
+  id: number;
+  message: string;
+  productId: number;
+  updatedAt: string;
+  userId: number;
+}
+interface ChatStructureType {
+  createdAt: string;
+  id: string;
+  messages: Array<MessageStructureType>
+  productId: string;
+  updatedAt: string;
+  user: {
+    id: number;
+    name: string;
+    avatar?: string;
+  }
+  userId: number;
+}
+
+const Chats = () => {
+  const router = useRouter();
+  const chat_data = useSWR<Array<ChatStructureType>>(`/api/chats`, getChatList);
+
+  const moveRouter = (chat_id: string, product_id: string) => {
+    router.push({
+      pathname: `/chats/${chat_id}`,
+      query: {
+        chat_id,
+        product_id,
+      }
+    })
+  }
+
+  if (chat_data.error) return <div>...에러</div>
+  if (!chat_data.data) return <div>...로딩중</div>
+
   return (
     <Layout hasTabBar title="채팅">
       <div className="divide-y-[1px] ">
-        {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-          <Link href={`/chats/${i}`} key={i}>
+        {chat_data.data.map((chat_info, i) => (
+          <div onClick={() => moveRouter(chat_info.id, chat_info.productId)} key={i}>
             <a className="flex px-4 cursor-pointer py-3 items-center space-x-3">
               <div className="w-12 h-12 rounded-full bg-slate-300" />
               <div>
-                <p className="text-gray-700">Steve Jebs</p>
+                <p className="text-gray-700">{chat_info.user.name}</p>
                 <p className="text-sm  text-gray-500">
-                  See you tomorrow in the corner at 2pm!
+                  {chat_info.messages[0]?.message}
                 </p>
               </div>
             </a>
-          </Link>
+          </div>
         ))}
       </div>
     </Layout>
