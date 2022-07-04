c6df12868afd3069a3ff63281178f3191c48bc90
wolfhoons <wolfhoons@gmail.com>
2022-07-05 02:12:29 +0900

    ✨ 등록한 날짜와 현재 날짜 비교 함수 라이브러리 에 추가

---
+++ src/libs/format.tsx
@@ -0,0 +1,20 @@
+export const getPrevDate = (date: Date) => {
+  const today = new Date();
+  const timeValue = new Date(date);
+
+  const betweenTime = Math.floor((today.getTime() - timeValue.getTime()) / 1000 / 60);
+  if (betweenTime < 1) return '방금 전';
+  if (betweenTime < 60) {
+      return `${betweenTime}분 전`;
+  }
+
+  const betweenTimeHour = Math.floor(betweenTime / 60);
+  if (betweenTimeHour < 24) {
+      return `${betweenTimeHour}시간 전`;
+  }
+
+  const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
+  if (betweenTimeDay < 365) {
+      return `${betweenTimeDay}일 전`;
+  }
+};
