52b301d41bc34d6c779592fc9efc21c047a88758
wolfhoons <wolfhoons@gmail.com>
2022-07-07 23:31:20 +0900

    🔥  공백 삭제

--- src/libs/format.tsx
+++ src/libs/format.tsx
@@ -5,16 +5,16 @@ export const getPrevDate = (date: Date) => {
   const betweenTime = Math.floor((today.getTime() - timeValue.getTime()) / 1000 / 60);
   if (betweenTime < 1) return '방금 전';
   if (betweenTime < 60) {
-      return `${betweenTime}분 전`;
+    return `${betweenTime}분 전`;
   }
 
   const betweenTimeHour = Math.floor(betweenTime / 60);
   if (betweenTimeHour < 24) {
-      return `${betweenTimeHour}시간 전`;
+    return `${betweenTimeHour}시간 전`;
   }
 
   const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
   if (betweenTimeDay < 365) {
-      return `${betweenTimeDay}일 전`;
+    return `${betweenTimeDay}일 전`;
   }
 };
