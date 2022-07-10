0b49d49d112c249af9e4cf4a36f03d5222b16b34
wolfhoons <wolfhoons@gmail.com>
2022-07-10 13:30:05 +0900

    💄 프로필 수정 페이지 스타일 수정 - 작업중

--- src/pages/profile/edit.tsx
+++ src/pages/profile/edit.tsx
@@ -2,11 +2,11 @@ import type { NextPage } from 'next';
 import { useForm } from 'react-hook-form';
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
-import Layout from 'src/components/Layout';
-import Input from 'src/components/Input';
-import Button from 'src/components/Button';
 import { getUserDetail, updateUser } from 'src/api/user';
 import useSWR from 'swr';
+import styled from 'styled-components';
+import Appbar from '@components/Layout/Appbar';
+import { getCFToken } from 'src/api/cloudflare';
 
 interface EditProfileForm {
   name: string;
@@ -16,46 +16,54 @@ interface EditProfileForm {
   formErrors?: string;
 }
 
+const ProfileEditContainer = styled.form`
+  margin-top: 5rem;
+`;
+
+const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 2rem;
+`;
+
 const EditProfile: NextPage = () => {
   const user_data = useSWR('/api/users/me', getUserDetail);
-  const {
-    register,
-    setValue,
-    handleSubmit,
-    setError,
-    watch,
-    formState: { errors },
-  } = useForm<EditProfileForm>();
+  const { register, setValue, handleSubmit, setError, watch } = useForm<EditProfileForm>();
   const avatar = watch('avatar');
-  const [avatar_priveiw, set_avatar_priview] = useState('');
+  const [avatar_preview, set_avatar_preview] = useState('');
+  const [error, set_error] = useState(false);
 
   useEffect(() => {
     if (!user_data.data) return;
     setValue('name', user_data.data.name);
     setValue('email', user_data.data.email);
     setValue('phone', user_data.data.phone);
-    set_avatar_priview(user_data.data.avatar);
-  }, [setValue, user_data.data]);
+    // set_avatar_preview(user_data.data.avatar);
+  }, [setValue, user_data]);
 
   useEffect(() => {
     if (avatar && avatar.length > 0) {
       const file = avatar[0];
-      set_avatar_priview(URL.createObjectURL(file));
+      set_avatar_preview(URL.createObjectURL(file));
     }
   }, [avatar]);
 
   if (user_data.error) return <div>...에러</div>;
   if (!user_data.data) return <div>...로딩중</div>;
-
+  console.log(avatar);
   const onUpdateUser = async ({ email, phone, name, avatar }: EditProfileForm) => {
-    if (email === '' && phone === '' && name === '') {
-      return setError('formErrors', {
-        message: 'Email OR Phone number are required. You need to choose one.',
-      });
+    console.log('진입');
+
+    if (email === '' || name === '') {
+      set_error(true);
+      return;
     }
 
+    console.log(email, phone, name, avatar);
+
     if (avatar && avatar.length > 0) {
-      const { uploadURL } = await (await fetch(`/api/files`)).json();
+      const { uploadURL } = await getCFToken();
 
       const form = new FormData();
       form.append('file', avatar[0], user_data.data?.id + '');
@@ -81,54 +89,46 @@ const EditProfile: NextPage = () => {
           alert('Profile Update Failure, Retry Update Please');
         });
     } else {
-      updateUser({ email, phone, name });
+      updateUser({ email, phone, name })
+        .then(() => {
+          alert('Profile Update Success');
+        })
+        .catch(() => {
+          alert('Profile Update Failure, Retry Update Please');
+        });
     }
   };
 
   return (
-    <Layout canGoBack title="Edit Profile">
-      <form onSubmit={handleSubmit(onUpdateUser)} className="py-10 px-4 space-y-4">
-        <div className="flex items-center space-x-3">
-          {avatar_priveiw ? (
-            <Image
-              src={`https://imagedelivery.net/PvvqDlv-2VYHUsYbyy-DlQ/${avatar_priveiw}/avatar`}
-              className="w-14 h-14 rounded-full bg-slate-500"
-              alt=""
-              layout="fill"
-            />
-          ) : (
-            <div className="w-14 h-14 rounded-full bg-slate-500" />
-          )}
-          <label
-            htmlFor="picture"
-            className="cursor-pointer py-2 px-3 border hover:bg-gray-50 border-gray-300 rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 text-gray-700"
-          >
-            Change
-            <input {...register('avatar')} id="picture" type="file" className="hidden" accept="image/*" />
-          </label>
-        </div>
-        <Input register={register('name', { required: true })} required={false} label="Name" name="name" type="text" />
-        <Input
-          register={register('email', { required: true })}
-          required={false}
-          label="Email address"
-          name="email"
-          type="email"
-        />
-        <Input
-          register={register('phone')}
-          required={false}
-          label="Phone number"
-          name="phone"
-          type="text"
-          kind="phone"
-        />
-        {errors.formErrors ? (
-          <span className="my-2 text-red-500 font-medium text-center block">{errors.formErrors.message}</span>
-        ) : null}
-        <Button content={'Update profile'} />
-      </form>
-    </Layout>
+    <>
+      <Appbar title="프로필" />
+      <ProfileEditContainer onSubmit={handleSubmit(onUpdateUser)}>
+        <FormContainer>
+          <div>
+            {avatar_preview ? (
+              <Image src={avatar_preview} width={280} height={280} layout="responsive" />
+              ) : (
+              <Image
+                src={`${process.env.NEXT_PUBLIC_CF_IMAGE}/${user_data.data.avatar}/avatar`}
+                alt=""
+                width={280}
+                height={280}
+                layout="responsive"
+              />
+            )}
+            <label htmlFor="picture">
+              Change
+              <input {...register('avatar')} id="picture" type="file" className="hidden" accept="image/*" />
+            </label>
+          </div>
+          <input {...register('name', { required: true })} required={false} name="name" type="text" />
+          <input {...register('email', { required: true })} required={false} name="email" type="email" />
+          {/* <input {...register('phone')} required={false} name="phone" type="text" /> */}
+          {error && <span>확인</span>}
+          <button type="submit">업데이트</button>
+        </FormContainer>
+      </ProfileEditContainer>
+    </>
   );
 };
 
