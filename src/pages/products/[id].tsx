d846a8545acd2def9e44a53908562366aefb6440
wolfhoons <wolfhoons@gmail.com>
2022-07-10 13:29:52 +0900

    💄 상품 상세 페이지 스타일 분리

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,158 +1,39 @@
 import React, { useEffect, useRef, useState } from 'react';
-import styled from 'styled-components';
 import { useRouter } from 'next/router';
 import { useSWRConfig } from 'swr';
 import useSWRImmutable from 'swr/immutable';
 import Link from 'next/link';
 import Image from 'next/image';
-// prisma
-import { Product, User } from '@prisma/client';
 // components
+import Appbar from '@components/Layout/Appbar';
 // api
 import { getProductDetail } from 'src/api/product';
 import { updateFavorite } from 'src/api/favorite';
-// lib
 import { createChat } from 'src/api/chat';
+// lib
+import { currencify, getPrevDate } from '@libs/format';
+// style
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import Appbar from '@components/Layout/Appbar';
-import { getPrevDate } from '@libs/format';
-
-interface ProductWithuser extends Product {
-  user: User;
-}
-interface ItemDetailResponse {
-  product: ProductWithuser;
-  relatedProducts: Product[];
-  isLiked: boolean;
-}
-
-export const DetailContainer = styled.div`
-  padding-top: 5rem;
-`;
-
-export const RegisterContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 2rem;
-  font-size: 1.4rem;
-  line-height: 2.8rem;
-  strong {
-    font-size: 1.6rem;
-    font-weight: var(--weight-600);
-  }
-
-  b {
-    color: var(--gray-2);
-  }
-`;
-
-const RegisterProfileContainer = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 2rem;
-  border-bottom: 1px solid var(--gray-1);
-  div {
-    display: flex;
-    flex-direction: column;
-    padding-left: 1rem;
-    font-size: 1.6rem;
-    p {
-      font-size: 1.2rem;
-      line-height: 2rem;
-      color: var(--gray-2);
-    }
-  }
-`;
-
-export const RegisterInfoContent = styled.div`
-  display: flex;
-  padding: 1rem 2rem;
-  border-top: 1px solid var(--gray-1);
-  border-bottom: 1px solid var(--gray-1);
-  div {
-    display: flex;
-    align-items: center;
-    p {
-      padding: 0 0.4rem 0 0.4rem;
-      font-size: 1.2rem;
-    }
-  }
-`;
-
-const RegisterImage = styled.div`
-  width: 100%;
-  padding-bottom: 2rem;
-`;
-
-const TabbarContainer = styled.div`
-  position: absolute;
-  bottom: 0;
-  right: 0;
-  left: 0;
-
-  height: 6.6rem;
-  width: 100%;
-
-  background-color: white;
-`;
-
-interface InnerWrapStyle {
-  width: number;
-}
-
-const TabbarInnerWrap = styled.div<InnerWrapStyle>`
-  position: fixed;
-  padding: 1rem 0rem;
-  width: ${({ width }) => width && `${width}px`};
-  border-top: 1px solid var(--gray-2);
-`;
-
-const TabbarWrap = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 100%;
-  margin-left: 1rem;
-`;
-
-const TabbarItemBox = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  height: 4rem;
-
-  font-size: 1.4rem;
-  font-weight: var(--weight-500);
-  button {
-    font-size: 1.4rem;
-    font-weight: var(--weight-500);
-    border: none;
-    background-color: transparent;
-    svg {
-      position: relative;
-      top: 0.2rem;
-      font-size: 2rem;
-    }
-  }
-`;
+import {
+  DetailContainer,
+  DetailContent,
+  DetailImage,
+  DetailProfileContainer,
+  DetailProfileImage,
+  DetailProfileContent,
+  TabbarContainer,
+  TabbarWrap,
+  TabbarItemBox,
+} from 'assets/pages/products/detail/styles';
+import { ProductDetailStructure } from '@libs/type/product_type';
 
 const ItemDetail = () => {
   const router = useRouter();
-  const product_detail = useSWRImmutable<ItemDetailResponse>(`/api/products/${router.query.id}`, () =>
+  const product_detail = useSWRImmutable<ProductDetailStructure>(`/api/products/${router.query.id}`, () =>
     getProductDetail(router.query.id as string),
   );
   const { mutate } = useSWRConfig();
   const [toggle_fav, set_toggle_fav] = useState(product_detail.data ? product_detail.data.isLiked : false);
-
-  const ref = useRef<HTMLHeadingElement>(null);
-  const [parent_width, set_parent_width] = useState(0);
-
-  useEffect(() => {
-    if (!ref.current) return;
-
-    set_parent_width(ref.current.offsetWidth);
-  }, []);
 
   useEffect(() => {
     if (!product_detail.data) return;
@@ -163,7 +44,7 @@ const ItemDetail = () => {
   if (product_detail.error) return <div>...error</div>;
   if (!product_detail.data) return <div>...loading</div>;
 
-  const onUpdateLike = () => {
+  const onUpdateFav = () => {
     updateFavorite(router.query.id as string)
       .then(() => {
         set_toggle_fav(!toggle_fav);
@@ -175,12 +56,25 @@ const ItemDetail = () => {
       });
   };
 
+  const isMyProduct = () => {
+    if(!product_detail.data) return;
+
+    if(product_detail.data.product.userId === product_detail.data.product.user.id) return true;
+    else return false;
+  }
+
   const onCreateChat = () => {
+    if (isMyProduct()) {
+      router.push('/');
+      return;
+    }
     createChat(product_detail.data?.product.id as number)
-      .then(() => {
+      .then((v) => {
         alert('Success Create ChatRoom');
+        router.push(`/chats/${v.id}?chat_id${v.id}&product_id=${v.productId}`);
       })
-      .catch(() => {
+      .catch((v) => {
+        console.log(v);
         alert('Failed to Create ChatRoom');
       });
   };
@@ -189,70 +83,55 @@ const ItemDetail = () => {
     <>
       <Appbar />
       <DetailContainer>
-        <RegisterImage>
+        {/* 상품 이미지 */}
+        <DetailImage>
           <Image
-            src={`https://imagedelivery.net/PvvqDlv-2VYHUsYbyy-DlQ/${product_detail.data?.product?.image}/public`}
+            src={`${process.env.NEXT_PUBLIC_CF_IMAGE}/${product_detail.data?.product?.image}/public`}
             width={280}
             height={280}
             alt=""
             layout="responsive"
           />
-        </RegisterImage>
-        <RegisterProfileContainer>
-          {product_detail.data.product.user.avatar ? (
+        </DetailImage>
+        {/* 프로필 관련 */}
+        <DetailProfileContainer>
+          <DetailProfileImage>
             <Image
-              src={`https://imagedelivery.net/PvvqDlv-2VYHUsYbyy-DlQ/${product_detail.data?.product?.user?.avatar}/avatar`}
-              className="w-12 h-12 rounded-full bg-slate-300"
-              alt=""
-              width={48}
-              height={48}
+              src={
+                product_detail.data.product.user.avatar
+                  ? `${process.env.NEXT_PUBLIC_CF_IMAGE}/${product_detail.data?.product?.user?.avatar}/avatar`
+                  : `${process.env.NEXT_PUBLIC_CF_IMAGE}/74514a95-ce9a-471d-b000-b927ff295500/avatar`
+              }
+              alt="사람들이 올린 프로필 이미지 동그란 모양"
+              layout="fill"
+              style={{ borderRadius: '50%' }}
             />
-          ) : (
-            <div style={{ width: '6rem', height: '6rem', backgroundColor: 'red' }}></div>
-          )}
-          <div>
+          </DetailProfileImage>
+          <DetailProfileContent>
             <strong>{product_detail.data.product.user.name}</strong>
-            <Link passHref href={`/users/profiles/${product_detail.data.product.user.id}`}>
-              <p>View profile &rarr;</p>
-            </Link>
-          </div>
-        </RegisterProfileContainer>
-        <RegisterContent className="mt-5">
+          </DetailProfileContent>
+        </DetailProfileContainer>
+        <DetailContent className="mt-5">
           <strong>{product_detail.data.product.name}</strong>
           <b>{getPrevDate(product_detail.data.product.createdAt)}</b>
           <p>{product_detail.data.product.description}</p>
-        </RegisterContent>
-        {/* <div>
-          <h2 className="text-2xl font-bold text-gray-900">Similar items</h2>
-          <div className=" mt-6 grid grid-cols-2 gap-4">
-            {product_detail.data.relatedProducts.map((product) => (
-              <Link key={product.id} passHref href={`/products/${product.id}`}>
-                <div>
-                  <div className="h-56 w-full mb-4 bg-slate-300" />
-                  <h3 className="text-gray-700 -mb-1">{product.name}</h3>
-                  <span className="text-sm font-medium text-gray-900">${product.price}</span>
-                </div>
-              </Link>
-            ))}
-          </div>
-        </div> */}
+        </DetailContent>
       </DetailContainer>
-      <TabbarContainer ref={ref}>
-        <TabbarInnerWrap width={parent_width}>
+      {/* 가격 및 채팅 */}
+      <TabbarContainer>
           <TabbarWrap>
             <TabbarItemBox>
-              <button onClick={onUpdateLike}>
+              <button onClick={onUpdateFav}>
                 <FavoriteBorderIcon style={{ fill: toggle_fav ? 'red' : 'black' }} />
               </button>
-              <p>{product_detail.data.product.price}만원</p>
+              <p>{currencify(product_detail.data.product.price)}원</p>
             </TabbarItemBox>
             <TabbarItemBox>
               <button style={{ width: '140px', height: '40px' }} onClick={onCreateChat}>
-                채팅하기
+                {isMyProduct() ? '채팅 목록으로' : '채팅하기'}
               </button>
             </TabbarItemBox>
           </TabbarWrap>
-        </TabbarInnerWrap>
       </TabbarContainer>
     </>
   );
