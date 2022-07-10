0e1c8fa47bc59b67d902330175a17aa8f6703404
wolfhoons <wolfhoons@gmail.com>
2022-07-10 13:28:17 +0900

    ✨ 행정 위치를 얻기 위해 kakao map sdk 라이브러리 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,14 +20,22 @@ class MyDocument extends Document {
           <>
             {initialProps.styles}
             {sheet.getStyleElement()}
-            <link rel="stylesheet" as="style" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css" />
+            <link
+              rel="stylesheet"
+              as="style"
+              href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
+            />
+            <script
+              type="text/javascript"
+              src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APPKEY}&libraries=services`}
+            ></script>
             {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
             <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
             <script src="https://unpkg.com/react/umd/react.production.min.js"></script>
 
             <script src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"></script> */}
-          </>
+          </>,
         ],
       };
     } finally {
