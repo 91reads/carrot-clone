1cec95dba062eab8017f90377ba5fbc0f39428df
wolfhoons <wolfhoons@gmail.com>
2022-07-10 13:27:29 +0900

    ✨ useCoord hook 수정, 현 행정동 위치 가져오기

--- src/libs/client/useCoord.ts
+++ src/libs/client/useCoord.ts
@@ -1,25 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
-interface UseCoordState {
-  longitude: number | null;
-  latitude: number | null;
+declare global {
+  interface Window {
+    kakao: any;
+  }
 }
 
 export default function useCoords() {
-  const [coords, set_coords] = useState<UseCoordState>({
-    latitude: null,
-    longitude: null,
-  });
-
-  const onSuccess = ({
-    coords: { latitude, longitude },
-  }: GeolocationPosition) => {
-    set_coords({ latitude, longitude })
-  };
+  const [region, set_region] = useState<string>('');
+  const [session_region, set_session_region] = useState('');
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(onSuccess);
+    if (sessionStorage.getItem('region')) {
+      set_session_region(sessionStorage.getItem('region') as string);
+      return ;
+    }
+    
+    let geocoder = new window.kakao.maps.services.Geocoder();
+    let callback = function (result: any[], status: any) {
+      if (status === window.kakao.maps.services.Status.OK) {
+        set_region(result[0].region_3depth_name);
+        sessionStorage.setItem('region', result[0].region_3depth_name);
+      } else {
+        return '당근마켓';
+      }
+    };
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      geocoder.coord2RegionCode(position.coords.longitude, position.coords.latitude, callback);
+    });
   }, []);
 
-  return coords;
+  return region || session_region;
 }
