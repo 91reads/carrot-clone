f91d77550a005d0381af6c6277ada113bc42bbe7
wolfhoons <wolfhoons@gmail.com>
2022-07-12 14:53:27 +0900

    ✨ 가입 페이지 토큰 받는 동안 로딩 표시

--- src/pages/enter.tsx
+++ src/pages/enter.tsx
@@ -9,7 +9,8 @@ import { requestOTP, verifyOTP } from 'src/api/auth';
 // assets
 import Logo from 'public/logo.png';
 // styles
-import { EnterContainer, EnterTitle, CustomInput } from 'assets/pages/enter/styles';
+import { EnterContainer, EnterTitle, CustomInput, EnterImage } from 'assets/pages/enter/styles';
+import Loading from '@components/Loading/Loading';
 
 interface EnterForm {
   email: string;
@@ -26,6 +27,7 @@ const Enter = () => {
 
   const [token_valid, set_token_vaild] = useState(false);
   const [active_color, set_active_color] = useState(false);
+  const [loading, set_loading] = useState(false);
 
   const watch_email = watch('email');
 
@@ -39,10 +41,12 @@ const Enter = () => {
   // 로그인 이메일 작성
   const onValid = (data: EnterForm) => {
     if (token_valid || !active_color) return;
+    set_loading(true);
 
     requestOTP(data)
       .then((data) => {
         set_token_vaild(true);
+        set_loading(false);
         setValue('token', data.payload);
         sessionStorage.setItem('userId', data.userId);
       })
@@ -66,7 +70,9 @@ const Enter = () => {
 
   return (
     <EnterContainer>
-      <Image src={Logo} width="140" height="146" alt="" />
+      <EnterImage>
+        <Image src={Logo} width="140" height="146" alt="" />
+      </EnterImage>
       <EnterTitle>
         <h3>안녕하세요!</h3>
         <h3>이메일을 입력해 주세요.</h3>
@@ -76,14 +82,18 @@ const Enter = () => {
         {/* 이메일 입력 폼 */}
         <form onSubmit={handleSubmit(onValid)}>
           <CustomInput {...register('email', { required: true })} type="email" />
-          <Button
-            content={token_valid ? '인증번호 확인중' : '인증번호 받기'}
-            marginTop={1}
-            active={active_color}
-            activeColor={token_valid ? 'var(--gray-2)' : 'var(--gray-4)'}
-            normalColor={'var(--gray-2)'}
-            disabled={token_valid}
-          />
+          {loading ? (
+            <Loading />
+          ) : (
+            <Button
+              content={token_valid ? '인증번호 확인중' : '인증번호 받기'}
+              marginTop={1}
+              active={active_color}
+              activeColor={token_valid ? 'var(--gray-2)' : 'var(--gray-4)'}
+              normalColor={'var(--gray-2)'}
+              disabled={token_valid}
+            />
+          )}
         </form>
 
         {/* 토큰 검증 폼 */}
