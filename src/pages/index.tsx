e240a57497c4724299c07382cd7d1bacca88b256
wolfhoons <wolfhoons@gmail.com>
2022-07-15 01:45:22 +0900

    제품 카드에 필터 추가 - 검색, 상태

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,18 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 import { Product } from '@prisma/client';
 import styled from 'styled-components';
 import useSWR from 'swr';
 // components
-import FloatingButton from 'src/components/FloattingButton';
-import ProductCard from '@components/Card/Product/ProductCard';
-// api
-import { getProductList } from 'src/api/product';
-// assets
 import Appbar from 'src/components/Layout/Appbar';
 import Tabbar from 'src/components/Layout/Tabbar';
-import { useRouter } from 'next/router';
-import { useState } from 'react';
-import useCoords from '@libs/client/useCoord';
 import Loading from '@components/Loading/Loading';
+import ProductCard from '@components/Card/Product/ProductCard';
+import FloatingButton from 'src/components/FloattingButton';
+// api
+import { getProductList } from 'src/api/product';
+// lib
+import useCoords from '@libs/client/useCoord';
 
 export interface ProductWithCount extends Product {
   _count: {
@@ -25,7 +25,6 @@ const HomeContainer = styled.div`
   padding-top: 5rem;
 `;
 
-
 const Home = () => {
   const product_data = useSWR<Array<ProductWithCount>>('/api/products', getProductList);
   const router = useRouter();
@@ -34,36 +33,44 @@ const Home = () => {
   const coord = useCoords();
 
   if (product_data.error) return <div>error</div>;
-  if (!product_data.data) return <Loading />
+  if (!product_data.data) return <Loading />;
 
   const onMoveRouter = (id: number) => {
     router.push(`/products/${id}`);
   };
 
-  const onSearchFilter = (product: any) => {
+  const isSearch = (product: any) => {
     if (!watch_search) return true;
     if (product.name.includes(watch_search)) return true;
     else return false;
+  };
+
+  const isLive = (product: any) => {
+    if (product.status === 'live') return true;
+    else false;
   };
 
   return (
     <>
       <Appbar title={coord} set_watch_search={set_watch_search} search={search} set_search={set_search} />
       <HomeContainer>
-        {product_data.data.filter(onSearchFilter).map((product) => (
-          <ProductCard
-            id={product.id}
-            key={product.id}
-            title={product.name}
-            price={product.price}
-            image={product.image}
-            comments={product.chats.length}
-            hearts={product._count?.favs}
-            onClick={() => onMoveRouter(product.id)}
-            updatedAt={product.updatedAt}
-            watch_search={watch_search}
-          />
-        ))}
+        {product_data.data
+          .filter(isSearch)
+          .filter(isLive)
+          .map((product) => (
+            <ProductCard
+              id={product.id}
+              key={product.id}
+              title={product.name}
+              price={product.price}
+              image={product.image}
+              comments={product.chats.length}
+              hearts={product._count?.favs}
+              onClick={() => onMoveRouter(product.id)}
+              updatedAt={product.createdAt}
+              watch_search={watch_search}
+            />
+          ))}
       </HomeContainer>
       <FloatingButton href="/products/register" />
       <Tabbar />
