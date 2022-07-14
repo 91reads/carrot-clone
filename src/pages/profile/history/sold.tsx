79692cade36b1aca26a92324526bb841edc832b0
wolfhoons <wolfhoons@gmail.com>
2022-07-15 01:46:57 +0900

    ✨ 판매 내역 실 데이터 적용

--- src/pages/profile/history/sold.tsx
+++ src/pages/profile/history/sold.tsx
@@ -5,34 +5,43 @@ import ProductCard from '@components/Card/Product/ProductCard';
 import Appbar from '@components/Layout/Appbar';
 import styled from 'styled-components';
 import Loading from '@components/Loading/Loading';
+import { ProductWithCount } from 'src/pages';
+import { getProductList } from 'src/api/product';
+import useUser from '@libs/client/useUser';
 
 const SoldContainer = styled.div`
   margin-top: 5rem;
 `;
 
 const Sold: NextPage = () => {
-  const history_data = useSWR(`/api/users/me/sales`, () => getUserHistory('sales'));
+  // const history_data = useSWR(`/api/users/me/sales`, () => getUserHistory('sales'));
+  const product_data = useSWR<Array<ProductWithCount>>('/api/products', getProductList);
+  const user_id = useUser();
 
-  if (history_data.error) return <div>...에러</div>;
-  if (!history_data.data) return  <Loading />
+  if (product_data.error) return <div>...에러</div>;
+  if (!product_data.data) return  <Loading />
 
-  console.log(history_data.data);
+  const isMyProduct = (product: any) => {
+    if(product.userId === user_id) return true
+    else false;
+  }
 
   return (
     <>
       <Appbar title="판매 내역" />
       <SoldContainer>
-        {history_data.data.map((v: any) => {
+        {product_data.data.filter(isMyProduct).map((v: any) => {
           return (
             <ProductCard
-              key={v.product.id}
-              title={v.product.name}
-              id={v.product.id}
-              price={v.product.price}
-              comments={v.product.chats.length}
-              hearts={v.product._count.favs}
-              image={v.product.image}
-              updatedAt={v.product.updatedAt}
+              key={v.id}
+              title={v.name}
+              id={v.id}
+              price={v.price}
+              comments={v.chats.length}
+              hearts={v._count.favs}
+              image={v.image}
+              updatedAt={v.updatedAt}
+              status={v.status}
             />
           );
         })}
