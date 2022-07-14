1763f60065975868f5e7217f8fa4133c5ab8a6ce
wolfhoons <wolfhoons@gmail.com>
2022-07-15 01:46:14 +0900

    [백앤드] 제품 상태 업데이트

---
+++ src/pages/api/products/update.tsx
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import withHandler, { ResponseType } from 'src/libs/server/withHandler';
+import client from 'src/libs/client/client';
+import { withApiSession } from 'src/libs/server/withSession';
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+
+  // 상품 등록
+  if (req.method === 'POST') {
+    const {
+      body: { product_id, status },
+      session: { user },
+    } = req;
+
+    await client.product.update({
+      where: {
+        id: Number(product_id),
+      },
+      data: {
+        status: status,
+      },
+    });
+    res.json({ ok: true });
+  }
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ['POST'],
+    handler,
+    isPrivate: true,
+  }),
+);
